6f1c2abb6337b9f0a09098a70fc604349711f830
Dumitru Tabara <dumitru.tabara@codwer.com>
2019-10-05 12:22:56 +0300

    Initial Commit

---
+++ UIViewModel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViewModel
+//
+//  Created by Dumitru Tabara on 10/5/19.
+//  Copyright © 2019 Dumitru Tabara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
