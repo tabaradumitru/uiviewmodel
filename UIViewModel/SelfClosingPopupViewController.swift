7b0b3a9a7de5ed1436ca50703a3f528a282a7a60
Dumitru Tabara <dumitru.tabara@codwer.com>
2020-01-18 14:29:21 +0200

    finished laborator nr1 at PAM

---
+++ UIViewModel/SelfClosingPopupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SelfClosingPopupViewController.swift
+//  UIViewModel
+//
+//  Created by Dumitru Tabara on 10/8/19.
+//  Copyright © 2019 Dumitru Tabara. All rights reserved.
+//
+
+import UIKit
+
+class SelfClosingPopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UIViewModel/SimpleNotification.swift
@@ -0,0 +1,9 @@
+//
+//  SimpleNotification.swift
+//  UIViewModel
+//
+//  Created by Dumitru Tabara on 1/9/20.
+//  Copyright © 2020 Dumitru Tabara. All rights reserved.
+//
+
+import Foundation
